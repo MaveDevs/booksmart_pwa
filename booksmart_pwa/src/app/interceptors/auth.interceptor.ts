2f5d30acc6b81dbaf52c66bf308634891efcba91
Isaac Gonzalez <desarrollo.fullstack3@atomicsolutions.mx>
2026-04-14 12:39:44 -0700

    Features

--- booksmart_pwa/src/app/interceptors/auth.interceptor.ts
+++ booksmart_pwa/src/app/interceptors/auth.interceptor.ts
@@ -1,12 +1,30 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { Auth } from '../services/auth/auth';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const auth = inject(Auth);
+  
   const token = localStorage.getItem('access_token');
+  let request = req;
+
   if (token) {
-    const cloned = req.clone({
+    request = req.clone({
       setHeaders: { Authorization: `Bearer ${token}` },
     });
-    return next(cloned);
   }
-  return next(req);
+
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      // Si el servidor responde 401 (Prohibido/Token vencido)
+      if (error.status === 401) {
+        auth.logout(); // Limpiamos el storage local
+        router.navigate(['/login']); // Redirigimos al login
+      }
+      return throwError(() => error);
+    })
+  );
 };
